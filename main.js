8520b760b50faa5d87cf32514c6eeea5b3b0b64e
darksystem <darksystemcoder@gmail.com>
2023-07-26 19:03:42 -0400

    js

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
-let users = lightdm.users
-lightdm.show_prompt.connect((text, type) => {
+//let users = lightdm.users
+/*function login(user,passwd,sess){
+  lightdm.show_prompt.connect((text, type) => {
     if (type == 0) // Login
-      lightdm.respond("user");
+      lightdm.respond(user);
     else if (type == 1) // Password
-      lightdm.respond("password");
+      lightdm.respond(passwd);
   })
   lightdm.authentication_complete.connect(() => {
-    lightdm.start_session("session");
+    lightdm.start_session(sess);
   })
   
 lightdm.authenticate();
+}*/
+
+function forEach(list,fn){
+    //console.log(list)
+    for(var i=0;i<Object.keys(list).length;i++){
+      //console.log(list[i],i,list)
+      fn(list[i],i,list)
+    }
+  }
+function bindMulEv(ev,cssClass,handler){
+  
+  let list = document.getElementsByClassName(cssClass)
+  
+  forEach(list,(elm)=>{
+    //console.log('bind')
+    elm.addEventListener(ev,handler)
+  })
+}
+async function getForm(){
+  return new Promise((resolve,reject)=>{
+    bindMulEv('click','passwdEv',(e)=>{
+      e.preventDefault()
+      resolve(document.getElementsByClassName('passwdEvFm')[0].value)
+    })
+    bindMulEv('submit','passwdEvFm',(e)=>{
+      e.preventDefault()
+      resolve(document.getElementsByClassName('passwdEvFm')[0].value)
+    })
+  })
+}
+(async()=>{
+  console.log(await getForm())
+})()
